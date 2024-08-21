5a02ecfc89dffafd395ae9895a0c4bec9d443360
Soso <soso.beriashvili@redberry.ge>
2024-08-21 16:47:38 +0400

    style: style main markup for signup form

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatCardModule],
+  imports: [MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
-export class SignupComponent {
-}
+export class SignupComponent {}
