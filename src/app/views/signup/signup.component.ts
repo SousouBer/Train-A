b5240249b4e1d2482f69b8910b28e9225298bd69
Soso <soso.beriashvili@redberry.ge>
2024-08-22 16:56:53 +0400

    feat: add custom validation to validate passwords

--- src/app/views/signup/signup.component.ts
+++ src/app/views/signup/signup.component.ts
@@ -5,11 +5,25 @@ import { LayoutsAuthFormComponent } from '../../layouts/layouts-auth-form/layout
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+
+function passwordsMatch(
+  control: AbstractControl
+): { [s: string]: boolean } | null {
+  const password = control.get('password')?.value;
+  const passwordConfirmation = control.get('password_confirmation')?.value;
+
+  if (password === passwordConfirmation) {
+    return null;
+  }
+
+  return { passwordsMismatch: true };
+}
 
 @Component({
   selector: 'app-signup',
@@ -25,17 +39,22 @@ import {
   styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-  signupForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(8),
-    ]),
-    password_confirmation: new FormControl('', [
-      Validators.required,
-      Validators.minLength(8),
-    ]),
-  });
+  signupForm = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+      password_confirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+      ]),
+    },
+    {
+      validators: passwordsMatch,
+    }
+  );
 
   onSubmit() {
     console.log(this.signupForm.value);
