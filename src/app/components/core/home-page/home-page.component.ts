38508c492fab081d4f4bbfa7a08c7575042a1866
Soso <soso.beriashvili@redberry.ge>
2024-08-29 17:36:36 +0400

    wip(style): style part of trip component

--- src/app/components/core/home-page/home-page.component.ts
+++ src/app/components/core/home-page/home-page.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { HomeSearchComponent } from './home-search/home-search.component';
 import { CitiesService } from '../../../services/cities.service';
 import { map } from 'rxjs';
+import { TripComponent } from './trip/trip.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [HomeSearchComponent],
+  imports: [HomeSearchComponent, TripComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
---
+++ src/app/components/core/home-page/trip/trip.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trip',
+  standalone: true,
+  imports: [],
+  templateUrl: './trip.component.html',
+  styleUrl: './trip.component.css'
+})
+export class TripComponent {
+
+}
